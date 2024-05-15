d5a51bbdcddff9dff4c1428b4a7b1f44cc18e24d
OmarMoleroGimeno <145361662+OmarMoleroGimeno@users.noreply.github.com>
2024-05-15 11:39:31 +0200

    15/05/2024

--- JavaScript/exercicis/exercici02/exercicio2.js
+++
@@ -1,9 +0,0 @@
-
-    let fechaActual = new Date();
-
-    document.getElementById("titol").innerHTML = "Hola, soc Omar i estic modificant aquest missatge";
-
-    window.alert("Benvingut a la segona pràctica");
-
-    console.log("Omar");
-    console.log(fechaActual);
--- JavaScript/pruebas/TablaDeMultiplicar.js
+++
@@ -1,5 +0,0 @@
-for (let i = 1; i < 2; i++) {
-    for (let j = 0; j < 11; j++) {
-        console.log(i + " x " + j + " = " + (i*j));
-    }
-}
